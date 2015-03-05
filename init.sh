40b7cdd382c06b8cf9eb214e8cb05b1d6eddf327
tobyroseman <tobyroseman@users.noreply.github.com>
2015-03-05 13:30:39 -0800

    another system package

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
-sudo apt-get install --yes emacs23 gnome-tweak-tool git python-pip
+#!/bin/bash
+set -e
+
+sudo apt-get install --yes emacs23 emacs-goodies-el gnome-tweak-tool git python-pip
 
 sudo pip install ipython
