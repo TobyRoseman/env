93f9ab0394d6de31b981b02b6de3461de5d80443
tobyroseman <tobyroseman@users.noreply.github.com>
2015-03-05 18:31:59 -0800

    change shell

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install --yes emacs23 emacs-goodies-el gnome-tweak-tool git python-pip
+sudo apt-get install --yes emacs23 emacs-goodies-el gnome-tweak-tool git python-pip zsh
 
 sudo pip install ipython
+
+chsh -s /usr/bin/zsh
+
+echo "[DONE]"
