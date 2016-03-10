921dfcc8735bb28fcc5a4a54452ad6014a77f40e
TobyRoseman <toby@dato.com>
2016-03-10 15:48:46 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install --yes emacs23 emacs-goodies-el gnome-tweak-tool git python-pip zsh
+sudo apt-get install --yes emacs23 emacs-goodies-el gnome-tweak-tool git htop parallel python-pip zsh
 
 sudo pip install ipython
 
 chsh -s /usr/bin/zsh
 
 cp ./.emacs ./zshrc ~
+
+# Allow SSH into this computer
+# sudo apt-get install openssh-server
+# sudo restart ssh
 
 echo "[DONE]"
