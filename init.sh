cfb285eb9d0f0399611b42729dc43cfc30a93398
TobyRoseman <toby@dato.com>
2016-04-15 15:22:34 -0700

    More system packages

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo apt-get update
 
-sudo apt-get install --yes emacs24 emacs-goodies-el gnome-tweak-tool git htop parallel python-pip zsh
+sudo apt-get install --yes build-essential emacs24 emacs-goodies-el gnome-tweak-tool git htop parallel python3-dev python-pip zsh
 
 sudo pip install ipython
 
