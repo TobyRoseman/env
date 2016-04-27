b4709188e30467bae2fb3b7b24a6f156f1d5327d
tobyroseman <toby@dato.com>
2016-04-27 16:59:00 -0700

    updated packages

--- init.sh
+++ init.sh
@@ -3,8 +3,10 @@ set -e
 
 sudo apt-get update
 
-sudo apt-get install --yes emacs24 emacs-goodies-el gnome-tweak-tool git htop parallel python-pip zsh
+sudo apt-get install --yes emacs24 emacs-goodies-el gnome-tweak-tool git htop moreutils parallel zsh
 
+# Python Stuff
+sudo apt-get install python-pip python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 sudo pip install ipython
 
 chsh -s /usr/bin/zsh
